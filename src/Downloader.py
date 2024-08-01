18b4c54c9583f9fde124e9684b4221c2e3c93f55
João Vale <carlosvalejcov@gmail.com>
2024-08-01 23:24:34 +0100

    bitrate converter added for music to be played on different devices

--- src/Downloader.py
+++ src/Downloader.py
@@ -1,10 +1,25 @@
 from pytube import YouTube
 import os
 import time
+import subprocess
 
+input_command = None
+output_command = None
+bitrate = None
+output_path = ""
 path = "/Users/jvale03/Documents" # update your path 
 
-def download_audio(url,output_path=""):
+
+def run_command():
+        command = f'ffmpeg -i "{input_command}" -b:a {bitrate} "{output_command}"' # 64 96 128 192 320
+        subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def sanitize_filename(filename):
+    return filename.replace(" ", "_").replace("(", "").replace(")", "").replace("&", "and")
+
+
+def download_audio(url, d_type):
+    global input_command, output_path, output_command
     try:
         # Creates YouTube object
         youtube = YouTube(url)
@@ -12,27 +27,38 @@ def download_audio(url,output_path=""):
         # Select the audio stream
         audio_stream = youtube.streams.filter(only_audio=True).first()
         
-        # Verificy if it youtube video exists
+        # Verify if the YouTube video exists and has audio streams
         if not audio_stream:
-            print("\033[31mNo audio streams found\033[m")
+            print("\033[31mNo audio streams found for this video\033[m")
             return
         
         # Define output path
         output_path = audio_stream.download(output_path=output_path)
         
-        # Rename youtube audios to have extension .mp3
+        # Rename YouTube audios to have extension .mp3
         base, ext = os.path.splitext(output_path)
         new_file = base + '.mp3'
         os.rename(output_path, new_file)
-        
-        audio_name = output_path.split("/")
+
+        audio_name = new_file.split("/")
+
+        if d_type != 0:
+            input_command = new_file
+            audio_name[-1] = sanitize_filename(audio_name[-1])
+            output_command = "/".join(audio_name)
+            try:
+                run_command()
+            except subprocess.CalledProcessError as e:
+                print(f"\033[31mError executing command: {e.stderr.decode()}.\033[m")
+                return
+
         print(f"\033[1mDownloaded:\033[m {audio_name[-1]}")
-        
+
     except Exception as e:
-        print(f"\033[31mError downloading audio: {e}\033[m")
+        print(f"\033[31mError downloading audio:  {str(e)}\033[m")
 
 
-def download_list(output_path,file="audio_list.txt"):
+def download_list(file="audio_list.txt"):
     # open file with audios list
     with open(file,"r") as f:
         content = f.readlines()
@@ -42,12 +68,13 @@ def download_list(output_path,file="audio_list.txt"):
     
     # download each audio of the file
     for audio in content:
-        download_audio(audio,output_path)
+        download_audio(audio,1)
 
     print(f"\033[32m\nDownload completed!\033[m")
 
 # method to define the path to save the audios
 def defining_path():
+    global output_path
     paths_list = []
     i=0
     print("\033[1mPaths: \033[m")
@@ -62,7 +89,7 @@ def defining_path():
         choice = input("\nChoose your path to save the audios: ")
         if choice.isdigit():
             choice = int(choice)
-            if choice > i or choice < 0:
+            if choice > i-1 or choice < 0:
                 print("\033[31mInvalid!\033[m")
             else:
                 break
@@ -73,10 +100,44 @@ def defining_path():
 
     if true_false == "y" or true_false == "":
         initial_time = time.time()
-        download_list(paths_list[choice])
+        output_path = paths_list[choice]
+        download_list()
         return time.time() - initial_time 
     
     return 0
+
+def defining_audio_quality():
+    global bitrate
+    choice = -1
+    while True:
+        choice = input("1: 320kbps (2,5MB/min)\n2: 192kbps (1,5MB/min)\n3: 128kbps (1MB/min)\n4: 96kbps (0,75MB/min)\n5: 64kbps (0,5MB/min)\n6: 32mbps (0,25MB/min)\n")
+        if choice.isdigit():
+            choice = int(choice)
+            if choice == 1:
+                bitrate = "320k"
+                break
+            elif choice == 2:
+                bitrate = "192k"
+                break
+            elif choice == 3:
+                bitrate = "128k"
+                break
+            elif choice == 4:
+                bitrate = "96k"
+                break
+            elif choice == 5:
+                bitrate = "64k"
+                break
+            elif choice == 6:
+                bitrate = "32k"
+                break
+
+            else:
+                print("\033[31mInvalid!\033[m")
+
+        else:
+            print("\033[31mInvalid!\033[m")
+
 
 def main():
     choice = 0
@@ -96,11 +157,13 @@ def main():
         url = input("Enter here your URL: ")
         print("---------------")
         initial_time = time.time()
-        download_audio(url)
+        download_audio(url,0)
         final_time = time.time() - initial_time
         print("---------------")
         print(f"It took {round(final_time,1)} seconds!")
     elif choice == 2:
+        print("---------------")
+        defining_audio_quality()
         print("---------------")
         final_time = defining_path()
         print("---------------")
